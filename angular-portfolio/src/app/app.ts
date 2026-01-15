4d206de6c96b1440cac30e6e441b2c1faa7e1028
Rogério Rocha <rogerio.sf.rocha@gmail.com>
2026-01-15 15:22:14 +0000

    Created all the sections needed + generated the components for each
    .

--- angular-portfolio/src/app/app.ts
+++ angular-portfolio/src/app/app.ts
@@ -1,11 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header'
+import { Landing } from './sections/landing/landing'
+import { About } from './sections/about/about'
+import { Education } from './sections/education/education'
+import { Experience } from './sections/experience/experience'
+import { Projects } from './sections/projects/projects'
+import { Contact } from './sections/contact/contact'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [Header],
+  imports: [Header,Landing,About,Education,Experience,Projects,Contact],
   styleUrl: './app.css'
 })
 export class App {

7bdb54a62d7b93a9ce99265830018770f916ee1a
Rogério Rocha <rogerio.sf.rocha@gmail.com>
2026-01-21 20:01:00 +0000

    Added assets and started work on the projects section
    .

--- angular-portfolio/src/app/sections/projects/projects.ts
+++ angular-portfolio/src/app/sections/projects/projects.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import {NgFor, NgIf} from '@angular/common';
+
+type Project = {
+  title: string;
+  description: string;
+  tags: string[];
+  repoUrl?: string;
+  liveUrl?: string;
+};
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [NgFor,NgIf],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects {
+  readonly heading = 'Projects';
+  readonly description =
+    'A list of past projects that demonstrate my skills and experience in software development, alongside what technologies I am proficient in.';
 
+  readonly items: Project[] = [
+    {
+      title: 'Personal Portfolio Website',
+      description:
+        'Description',
+      tags: ['Angular', 'TypeScript', 'Tailwind'],
+      repoUrl: 'https://github.com/RogerioFSR/Portfolio-Website',
+      liveUrl: ''
+    },
+    {
+      title: 'Personal Portfolio Website',
+      description:
+        'Description',
+      tags: ['Angular', 'TypeScript', 'Tailwind'],
+      repoUrl: 'https://github.com/RogerioFSR/Portfolio-Website',
+      liveUrl: ''
+    },
+    {
+      title: 'Personal Portfolio Website',
+      description:
+        'Description',
+      tags: ['Angular', 'TypeScript', 'Tailwind'],
+      repoUrl: 'https://github.com/RogerioFSR/Portfolio-Website',
+      liveUrl: ''
+    },
+    {
+      title: 'Personal Portfolio Website',
+      description:
+        'Description',
+      tags: ['Angular', 'TypeScript', 'Tailwind'],
+      repoUrl: 'https://github.com/RogerioFSR/Portfolio-Website',
+      liveUrl: ''
+    }
+  ];
 }
